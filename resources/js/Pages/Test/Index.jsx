0bfecb8a0543d7b9d4354e9b65906429ea25ad52
Dom <100814585+LeoDomGit@users.noreply.github.com>
2024-10-03 19:26:07 +0700

    Admin

---
+++ resources/js/Pages/Test/Index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../../Components/Layout'
+
+function Index() {
+  return (
+    <Layout>
+<>
+    Test
+    </>
+    </Layout>
+    
+  )
+}
+
+export default Index
