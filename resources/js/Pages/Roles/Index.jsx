1445f5ac8f25bd51415408a00a43382bb4ee8507
Dom <100814585+LeoDomGit@users.noreply.github.com>
2024-10-04 11:15:17 +0700

    Update Index.jsx

--- resources/js/Pages/Roles/Index.jsx
+++ resources/js/Pages/Roles/Index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import Layout from '../../components/Layout'
+import Layout from '../../Components/Layout'
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { Notyf } from 'notyf';
